27948303a75a9ec1f8d10ec99ec423b213c5b89d
Gaby Aoki <40901652+gabyaoki@users.noreply.github.com>
2018-09-17 15:03:15 -0700

    Add files via upload
    Basic Files

---
+++ partials/footer.php
@@ -0,0 +1,11 @@
+	<footer>
+		<div class="container">
+			<div id="navFooter">
+				<a class="btFooter" href="#"><img class="iconFooter" src="images/back.png" alt="back" />Back</a>
+				<a class="btFooter" href="#"><img class="iconFooter" src="images/home.png" alt="home" />Home</a>
+				<a class="btFooter" href="#"><img class="iconFooter" src="images/hubit.png" alt="hubit" />My hubs</a>
+			</div><!-- //navFooter -->
+		</div><!-- //container -->
+	</footer><!-- //footer -->
+</body>
+</html>
