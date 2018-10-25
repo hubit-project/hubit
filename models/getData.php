a7389bbcea502a4bbc5f777c48658e230ffe155d
VANARTS\icarillo <italo.carillo@globo.com>
2018-10-25 12:57:11 -0700

    view for all user hubs page applied

--- models/getData.php
+++ models/getData.php
@@ -3,7 +3,7 @@ function getData($purpose){
 	if(isset($_SESSION['nUserID'])){
 		$userInfos = "SELECT strFirstName, strProfilePhoto, strCoverPhoto FROM users WHERE id=".$_SESSION['nUserID'];
 		$userHubsIDs = "SELECT GROUP_CONCAT(nHubID) FROM hubs_users WHERE nUserID=".$_SESSION['nUserID'];
-		$hubsName = "SELECT strName FROM hubs WHERE hubs.id IN (".$userHubsIDs.")";
+		$hubsName = "SELECT strName, strCoverPhoto, id FROM hubs WHERE hubs.id IN (".$userHubsIDs.")";
 		$hubsPhotosIDs = "SELECT GROUP_CONCAT(nPhotoID) FROM users_posts WHERE nHubID IN ('".$userHubsIDs."')";
 		$arrUserLastFeeds = "SELECT * FROM feeds WHERE nPhotoID IN ('".$hubsPhotosIDs."') ORDER BY nUTCsubmited DESC LIMIT 5";
 		switch ($purpose){
--- views/allhubs.php
+++ views/allhubs.php
@@ -4,12 +4,37 @@ $userHubs = getData('allHubs');
 
 ?>
 <div class="contentWrapper container">
+	<div class="myHubsTitle">
+		<h2>My hubs</h2>
+	</div><!--myHubsTitle-->
 <?php
 	foreach($userHubs as $hub){
 ?>
-	<a href="index.php?controller=Page&action=hub"><?=$hub['strName']?></a><br/>
+	<div class="myHubHolder">
+		<div class="myHubCover coverBg">
+			<a href="index.php?controller=Page&action=hub&hubID=<?=$hub['id']?>">
+				<img src="assets/<?=$hub['strCoverPhoto']?>" alt="myhub">
+			</a>
+		</div><!--myHubCover-->
+
+		<div class="myHubHeadings">
+			<h3><?=$hub['strName']?></h3>
+			<p>(Admin/s Name)</p>
+		</div><!--myHubHeadings-->
+	</div><!--myHubHolder-->
 <?php
 	}
 ?>
-	<a href="index.php?controller=Page&action=createhub">Create New Hub</a>
+	<div class="myHubHolder">
+		<div class="myHubCover coverBg">
+			<a href="index.php?controller=Page&action=createhub">
+				<img src="images/add-hub-icon.png" alt="myhub">
+			</a>
+		</div><!--myHubCover-->
+
+		<div class="myHubHeadings">
+			<h3>Create new hub</h3>
+		</div><!--myHubHeadings-->
+	</div><!--myHubHolder-->
+
 </div>
